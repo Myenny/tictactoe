c059da0629e313d1194e0d7d0c003f4746c168fe
Benjamin JEHL <benjaminjehl@Benjamins-MBP.localdomain>
2018-09-20 17:09:23 -0400

    added event listeners and some comments

--- public/main.js
+++ public/main.js
@@ -1,5 +1,37 @@
+// log click count
+// if odd = x
+//    if click count !/ 2 = x
+// if even = y
+//    if click count / 2 = y
+// add text content to DOM
+// 
+
+
+
+const playerSelected = () => {
+  console.log('clicked')
+}
+
+
+
 const main = () => {
-  document.querySelector('.top-left').addEventListener('click', topLeftClicked)
+  document.querySelector('.one').addEventListener('click', playerSelected)
+
+  document.querySelector('.two').addEventListener('click', playerSelected)
+
+  document.querySelector('.three').addEventListener('click', playerSelected)
+
+  document.querySelector('.four').addEventListener('click', playerSelected)
+
+  document.querySelector('.five').addEventListener('click', playerSelected)
+
+  document.querySelector('.six').addEventListener('click', playerSelected)
+
+  document.querySelector('.seven').addEventListener('click', playerSelected)
+
+  document.querySelector('.eight').addEventListener('click', playerSelected)
+
+  document.querySelector('.nine').addEventListener('click', playerSelected)
 
 }
 
