492e77cb014d768614c8c33374a21965d292a786
Benjamin JEHL <benjaminjehl@Benjamins-MacBook-Pro.local>
2018-09-22 15:04:27 -0400

    we have a winner

--- public/main.js
+++ public/main.js
@@ -1,53 +1,8 @@
-// listen for a click === done
-// assign click count to 0
-// count that click
-// determine if odd or even click
-// if odd assign the value of X
-// if even assign the value of O
-// change the 'td' text content to either X or O
-//   make it go to the correct box
-//   somehow select each box and update text content when clicked
-//     listen for click for each individual box
-// do this 4 times
-// give a value of 1 to x
-// give a value of -1 to o
-// check for winner function
-//   if box 1 2 3 = x display winner
-//   if box 4 5 6 = x display winner
-//   if box 7 8 9 = x display winner
-//   if box 1 4 7 = x display winner
-//   if box 2 5 8 = x display winner
-//   if box 3 6 9 = x display winner
-//   if box 1 5 9 = x display winner
-//   if box 3 5 7 = x display winner
-//   if box 1 2 3 = o display winner
-//   if box 4 5 6 = o display winner
-//   if box 7 8 9 = o display winner
-//   if box 1 4 7 = o display winner
-//   if box 2 5 8 = o display winner
-//   if box 3 6 9 = o display winner
-//   if box 1 5 9 = o display winner
-//   if box 3 5 7 = o display winner
-//
-// if the value of x is 1 and i run a function to check all rows and columns and diagonals to equal 3
-// x wins
-//
-// if the value of 0 is -1 and i run a function to check all rows columns and diagonal to equal -3
-// o won
-//
-// if neither are true its a tie
-// select the element on the DOM add winner results
-// add check winner results to the DOM => textContent to h2
 
-// log click count
-// if odd = x
-//    if click count !/ 2 = x
-// if even = y
-//    if click count / 2 = y
-// add text content to DOM
-//
 let clickCount = 0
 let mainBoard
+let playerOneValue = []
+let playerTwoValue = []
 const playerOne = { letter: 'o', value: 1 }
 const playerTwo = { letter: 'x', value: -1 }
 const winCombos = [
@@ -61,8 +16,15 @@ const winCombos = [
   ['three', 'five', 'seven']
 ]
 
-const calculateValue = (value) => {
+const playerOneWinCondition = () => {
+  if (mainBoard.one === 'x' && mainBoard.two === 'x' && mainBoard.three === 'x') {
+    document.querySelector('.game-decision').textContent = 'Player One Wins!'
+  }
 
+}
+
+const checkWinner = () => {
+  playerOneWinCondition()
 }
 
 let box = document.querySelectorAll('.box')
@@ -70,7 +32,7 @@ let box = document.querySelectorAll('.box')
 let newGame = () => {
   mainBoard = Array.from(Array(9).keys())
   for (let i = 0; i < box.length; i++) {
-    box[i].addEventListener('click', playerClick, false)
+    box[i].addEventListener('click', playerClick)
   }
 }
 
@@ -86,49 +48,42 @@ const playerClick = (square) => {
 
 const turn = (squareID, player) => {
   mainBoard[squareID] = player
-  document.getElementById(squareID).innerText = player
+  document.getElementById(squareID).textContent = player
   document.getElementById(squareID).removeEventListener('click', playerClick)
+  checkWinner()
 }
 
-const checkWinner = () => {
-  if (winCombos[0] === 3 ||
-    winCombos[1] === 3 ||
-    winCombos[2] === 3 ||
-    winCombos[3] === 3 ||
-    winCombos[4] === 3 ||
-    winCombos[5] === 3 ||
-    winCombos[6] === 3) {
-    document.querySelector('.game-decision').textContent = 'Player One Wins!'
-  }
-
-  if (winCombos[0] === 3 ||
-    winCombos[1] === 3 ||
-    winCombos[2] === 3 ||
-    winCombos[3] === 3 ||
-    winCombos[4] === 3 ||
-    winCombos[5] === 3 ||
-    winCombos[6] === 3) {
-    document.querySelector('.game-decision').textContent = 'Player Two Wins!'
-  }
-
-  if (winCombos[0] !== 3 ||
-    winCombos[1] !== 3 ||
-    winCombos[2] !== 3 ||
-    winCombos[3] !== 3 ||
-    winCombos[4] !== 3 ||
-    winCombos[5] !== 3 ||
-    winCombos[6] !== 3) {
-    document.querySelector('.game-decision').textContent = 'It is a draw. You both suck!'
-  }
-}
-// tabulate scores for every win condition
-// adding player value
-// has to be value of winning direction array
-//    === +3 or -3
-// declare a winner
-
-
-
+// const checkWinner = () => {
+//   if (winCombos[0] === 3 ||
+//     winCombos[1] === 3 ||
+//     winCombos[2] === 3 ||
+//     winCombos[3] === 3 ||
+//     winCombos[4] === 3 ||
+//     winCombos[5] === 3 ||
+//     winCombos[6] === 3) {
+//     document.querySelector('.game-decision').textContent = 'Player One Wins!'
+//   }
+// 
+//   if (winCombos[0] === 3 ||
+//     winCombos[1] === 3 ||
+//     winCombos[2] === 3 ||
+//     winCombos[3] === 3 ||
+//     winCombos[4] === 3 ||
+//     winCombos[5] === 3 ||
+//     winCombos[6] === 3) {
+//     document.querySelector('.game-decision').textContent = 'Player Two Wins!'
+//   }
+// 
+//   if (winCombos[0] !== 3 ||
+//     winCombos[1] !== 3 ||
+//     winCombos[2] !== 3 ||
+//     winCombos[3] !== 3 ||
+//     winCombos[4] !== 3 ||
+//     winCombos[5] !== 3 ||
+//     winCombos[6] !== 3) {
+//     document.querySelector('.game-decision').textContent = 'It is a draw. You both suck!'
+//   }
+// }
 
 const main = () => {
   document.querySelector('.sandbox').addEventListener('click', () => {
@@ -145,4 +100,3 @@ const main = () => {
 }
 
 document.addEventListener('DOMContentLoaded', main)
-
