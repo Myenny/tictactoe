ff7e952802c07c9e5a3d0f237573f95d248db389
Benjamin JEHL <benjaminjehl@Benjamins-MacBook-Pro.local>
2018-09-22 15:29:56 -0400

    finishing up draw scenario

--- public/main.js
+++ public/main.js
@@ -1,8 +1,8 @@
 
 let clickCount = 0
 let mainBoard
-let playerOneValue = []
-let playerTwoValue = []
+// let playerOneValue = []
+// let playerTwoValue = []
 const playerOne = { letter: 'o', value: 1 }
 const playerTwo = { letter: 'x', value: -1 }
 const winCombos = [
@@ -21,10 +21,79 @@ const playerOneWinCondition = () => {
     document.querySelector('.game-decision').textContent = 'Player One Wins!'
   }
 
+  if (mainBoard.four === 'x' && mainBoard.five === 'x' && mainBoard.six === 'x') {
+    document.querySelector('.game-decision').textContent = 'Player One Wins!'
+  }
+
+  if (mainBoard.seven === 'x' && mainBoard.eight === 'x' && mainBoard.nine === 'x') {
+    document.querySelector('.game-decision').textContent = 'Player One Wins!'
+  }
+
+  if (mainBoard.one === 'x' && mainBoard.four === 'x' && mainBoard.seven === 'x') {
+    document.querySelector('.game-decision').textContent = 'Player One Wins!'
+  }
+
+  if (mainBoard.two === 'x' && mainBoard.five === 'x' && mainBoard.eight === 'x') {
+    document.querySelector('.game-decision').textContent = 'Player One Wins!'
+  }
+
+  if (mainBoard.three === 'x' && mainBoard.six === 'x' && mainBoard.nine === 'x') {
+    document.querySelector('.game-decision').textContent = 'Player One Wins!'
+  }
+
+  if (mainBoard.one === 'x' && mainBoard.five === 'x' && mainBoard.nine === 'x') {
+    document.querySelector('.game-decision').textContent = 'Player One Wins!'
+  }
+
+  if (mainBoard.three === 'x' && mainBoard.five === 'x' && mainBoard.seven === 'x') {
+    document.querySelector('.game-decision').textContent = 'Player One Wins!'
+  }
+}
+
+const playerTwoWinCondition = () => {
+  if (mainBoard.one === 'o' && mainBoard.two === 'o' && mainBoard.three === 'o') {
+    document.querySelector('.game-decision').textContent = 'Player Two Wins!'
+  }
+
+  if (mainBoard.four === 'o' && mainBoard.five === 'o' && mainBoard.six === 'o') {
+    document.querySelector('.game-decision').textContent = 'Player Two Wins!'
+  }
+
+  if (mainBoard.seven === 'o' && mainBoard.eight === 'o' && mainBoard.nine === 'o') {
+    document.querySelector('.game-decision').textContent = 'Player Two Wins!'
+  }
+
+  if (mainBoard.one === 'o' && mainBoard.four === 'o' && mainBoard.seven === 'o') {
+    document.querySelector('.game-decision').textContent = 'Player Two Wins!'
+  }
+
+  if (mainBoard.two === 'o' && mainBoard.five === 'o' && mainBoard.eight === 'o') {
+    document.querySelector('.game-decision').textContent = 'Player Two Wins!'
+  }
+
+  if (mainBoard.three === 'o' && mainBoard.six === 'o' && mainBoard.nine === 'o') {
+    document.querySelector('.game-decision').textContent = 'Player Two Wins!'
+  }
+
+  if (mainBoard.one === 'o' && mainBoard.five === 'o' && mainBoard.nine === 'o') {
+    document.querySelector('.game-decision').textContent = 'Player Two Wins!'
+  }
+
+  if (mainBoard.three === 'o' && mainBoard.five === 'o' && mainBoard.seven === 'o') {
+    document.querySelector('.game-decision').textContent = 'Player Two Wins!'
+  }
+}
+
+let checkDraw = () => {
+  if (mainBoard.one && mainBoard.two && mainBoard.three && mainBoard.four && mainBoard.five && mainBoard.six && mainBoard.seven && mainBoard.eight && mainBoard.nine !== '') {
+    document.querySelector('.game-decision').textContent = 'Draw. You both suck!'
+  }
 }
 
 const checkWinner = () => {
   playerOneWinCondition()
+  playerTwoWinCondition()
+  checkDraw()
 }
 
 let box = document.querySelectorAll('.box')
