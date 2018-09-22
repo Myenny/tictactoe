f3c66fe09e2801287b7f7bb16496f11a614525d1
Benjamin JEHL <benjaminjehl@Benjamins-MacBook-Pro.local>
2018-09-22 14:27:53 -0400

    check winner added. can't add values yet

--- public/main.js
+++ public/main.js
@@ -48,8 +48,8 @@
 //
 let clickCount = 0
 let mainBoard
-const playerOne = 'o'
-const playerTwo = 'x'
+const playerOne = { letter: 'o', value: 1 }
+const playerTwo = { letter: 'x', value: -1 }
 const winCombos = [
   ['one', 'two', 'three'],
   ['four', 'five', 'six'],
@@ -60,6 +60,10 @@ const winCombos = [
   ['one', 'five', 'nine'],
   ['three', 'five', 'seven']
 ]
+
+const calculateValue = (value) => {
+
+}
 
 let box = document.querySelectorAll('.box')
 
@@ -72,17 +76,57 @@ let newGame = () => {
 
 const playerClick = (square) => {
   if (clickCount === 1 || clickCount === 3 || clickCount === 5 ||
-    clickCount === 7 || clickCount === 9) {
-    turn(square.target.id, playerOne)
+    clickCount === 7 || clickCount === 9 || clickCount === 11 || clickCount === 13 ||
+    clickCount === 15 || clickCount === 17 || clickCount === 19) {
+    turn(square.target.id, playerOne.letter)
   } else {
-    turn(square.target.id, playerTwo)
+    turn(square.target.id, playerTwo.letter)
   }
 }
 
 const turn = (squareID, player) => {
   mainBoard[squareID] = player
   document.getElementById(squareID).innerText = player
+  document.getElementById(squareID).removeEventListener('click', playerClick)
 }
+
+const checkWinner = () => {
+  if (winCombos[0] === 3 ||
+    winCombos[1] === 3 ||
+    winCombos[2] === 3 ||
+    winCombos[3] === 3 ||
+    winCombos[4] === 3 ||
+    winCombos[5] === 3 ||
+    winCombos[6] === 3) {
+    document.querySelector('.game-decision').textContent = 'Player One Wins!'
+  }
+
+  if (winCombos[0] === 3 ||
+    winCombos[1] === 3 ||
+    winCombos[2] === 3 ||
+    winCombos[3] === 3 ||
+    winCombos[4] === 3 ||
+    winCombos[5] === 3 ||
+    winCombos[6] === 3) {
+    document.querySelector('.game-decision').textContent = 'Player Two Wins!'
+  }
+
+  if (winCombos[0] !== 3 ||
+    winCombos[1] !== 3 ||
+    winCombos[2] !== 3 ||
+    winCombos[3] !== 3 ||
+    winCombos[4] !== 3 ||
+    winCombos[5] !== 3 ||
+    winCombos[6] !== 3) {
+    document.querySelector('.game-decision').textContent = 'It is a draw. You both suck!'
+  }
+}
+// tabulate scores for every win condition
+// adding player value
+// has to be value of winning direction array
+//    === +3 or -3
+// declare a winner
+
 
 
 
@@ -101,3 +145,4 @@ const main = () => {
 }
 
 document.addEventListener('DOMContentLoaded', main)
+
